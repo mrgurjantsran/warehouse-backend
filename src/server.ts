780cb02f86d2121cdf94fb09e8109c90a4ec9648
mrgurjantsran <mr.gurjantsran@gmail.com>
2025-11-09 12:04:44 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -17,17 +17,25 @@ const app: Express = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+// app.use(cors({
+//   origin: ['http://localhost:3000', 
+//            'http://localhost:3001',
+//            'https://divine-wms-ft.vercel.app',
+//            'https://divine-wms-g5tto378c-gurjant-srans-projects.vercel.app',
+//            'https://divine-wms-ft-git-main-gurjant-srans-projects.vercel.app'
+//           ],
+//   credentials: true,
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+//   allowedHeaders: ['Content-Type', 'Authorization']
+// }));
+
 app.use(cors({
-  origin: ['http://localhost:3000', 
-           'http://localhost:3001',
-           'https://divine-wms-ft.vercel.app',
-           'https://divine-wms-g5tto378c-gurjant-srans-projects.vercel.app',
-           'https://divine-wms-ft-git-main-gurjant-srans-projects.vercel.app'
-          ],
-  credentials: true,
+  origin: '*',
+  // credentials: true,   // *REMOVE THIS LINE/HATADO!*
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
 
 app.use(express.urlencoded({ limit: '500mb', extended: true }));
 app.use(express.json({ limit: '500mb' }));
@@ -83,6 +91,7 @@ app.use(errorHandler);
 })();
 
 export default app;
+
 
 
 
