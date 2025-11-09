63e6a5e8b1171dc6746f5ee458e820da845e6f8d
mrgurjantsran <mr.gurjantsran@gmail.com>
2025-11-09 09:40:00 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,10 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001'],
+  origin: ['http://localhost:3000', 
+           'http://localhost:3001',
+           'https://divine-wms-ft.vercel.app' 
+          ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
@@ -79,3 +82,4 @@ app.use(errorHandler);
 })();
 
 export default app;
+
